86b4b7df65f954cbd223f4c4bab8f98255335e1e
Sriharsha <harshasrisri@gmail.com>
2014-01-14 01:48:28 -0700

    first attempt at issuing instructions

--- tomasulo.c
+++ tomasulo.c
@@ -68,17 +68,61 @@ void create_arch()
 	}
 }
 
+RS *dep_lookup (char *name) {
+}
+
 void issue () {
-	int i;
+	int i, rs_count;
 	Instruction *curr;
+	RS *rs_type, *rs;
 	curr = &iq[instr_proc];
 
+	/* Based on the opcode type, assign a common looping pointer and a counter {{{ */ 
 	if (!(strcmp ("ADD", curr->opcd)) || !(strcmp ("SUB", curr->opcd))) {
-		for (i = 0; i < NUM_INT_ADD_RS; i++)
-			if (add_int_rs[i].status == AVAILABLE)
-				break;
-
+		rs_count = NUM_INT_ADD_RS; rs_type = add_int_rs;
 	}
+	else if (!(strcmp ("ADDD", curr->opcd)) || !(strcmp ("SUBD", curr->opcd))) {
+		rs_count = NUM_FLT_ADD_RS; rs_type = mul_int_rs;
+	}
+	else if (!(strcmp ("MUL", curr->opcd)) || !(strcmp ("DIV", curr->opcd))) {
+		rs_count = NUM_INT_MUL_RS; rs_type = add_fp_rs;
+	}
+	else if (!(strcmp ("MULD", curr->opcd)) || !(strcmp ("DIVD", curr->opcd))) {
+		rs_count = NUM_FLT_MUL_RS; rs_type = mul_fp_rs;
+	}
+	else if (!(strcmp ("LD", curr->opcd))) {
+		rs_count = NUM_LD_RS; rs_type = ld_rs;
+	}
+	else if (!(strcmp ("SD", curr->opcd))) {
+		rs_count = NUM_SD_RS; rs_type = sd_rs;
+	}
+	/* }}} */
+
+	/* Grab the first available RS of the type selected in the previous step {{{ */
+	for (i = 0; i < rs_count; i++)
+		if (rs_type[i].status == AVAILABLE)
+			break;
+
+	/* If all the RS are occupied, simply return back without incrementing */ 
+	/* the number of instructions executed. this causes the subsequent issue */
+	/* call to process the same instruction for issuing */ 
+
+	if (i >= rs_count) return;
+
+	/* }}} */
+
+	/* now, the instruction is ready to be issued. we can get started */
+	rs = &rs_type[i];
+	rs->status = BUSY;
+
+	if (curr->src1)
+		rs->qj = dep_lookup (curr->src1);
+
+	if (curr->src2)
+		rs->qk = dep_lookup (curr->src2);
+
+	curr->issue_time = cycles;
+	instr_proc++;	
 }
 
 int main (int argc, char **argv) {
@@ -92,8 +136,6 @@ int main (int argc, char **argv) {
 	create_arch();
 
 	while (instr_proc < instr_count) {
-
-		instr_proc++;	
 
 		cycles++;
 	}
